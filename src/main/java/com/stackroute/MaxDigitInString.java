e2b73db292a776ff52b48c2d3ba1dc736acdf5ea
Ubuntu <ubuntu@ip-172-31-41-20.ec2.internal>
2019-11-06 15:58:31 +0000

    Logical03

--- src/main/java/com/stackroute/MaxDigitInString.java
+++ src/main/java/com/stackroute/MaxDigitInString.java
@@ -1,14 +1,39 @@
 package com.stackroute;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MaxDigitInString {
 
 	public static int getMaxDigit(String alphanumeric) {
-		return -2;
+		String regex="\\d";
+		Pattern p=Pattern.compile(regex);
+		Matcher m=p.matcher(alphanumeric);
+		int max=0;
+		while(m.find()){
+			int num=Integer.parseInt(m.group());
+			if(num>max){
+				max=num;
+			}
+		}
+		if(alphanumeric==null || max==0){
+			return -1;
+		}
+		return max;
 	}
 
    	//Do not print anything other than what's expected/asked in problem
     public static void main(String[] args) {
-    	//Use Scanner to get input from console
+    	Scanner s=new Scanner(System.in);
+    	String input=s.nextLine();
+    	int result=MaxDigitInString.getMaxDigit(input);
+    	if(result>0){
+    		System.out.println("Maxdigit:"+result);
+    	}
+    	else{
+    		System.out.println("No digits in string");
+    	}
  
     
     }
